7bf1b52e4a82cf9b2f84e6e8626ace0be1fc9caa
Yeojin Kim <yeojin.dev@gmail.com>
2019-11-23 01:09:38 +0900

    SMIT - ICNet 네트워크 기타 함수 추가

--- smit/icnet/network.py
+++ smit/icnet/network.py
@@ -93,3 +93,66 @@ class Network:
 			output = activation(output)
 
 		return output
+
+	@layer
+	def activator(self, inputs, name=None):
+		return DEFAULT_ACTIVATION(inputs, name=name)
+
+	@layer
+	def max_pool(self, inputs, pool_size=2, strides=2, padding='SAME', name=None):
+		return tf.layers.max_pooling2d(
+			inputs=inputs,
+			pool_size=pool_size,
+			strides=strides,
+			padding=padding,
+			name=name,
+		)
+
+	@layer
+	def dense(self, inputs, units=1000, activation=None, use_bias=True, kernel_initializer=DEFAULT_INITIALIZER, name=None):
+		with tf.variable_scope(name):
+			output = tf.layers.dense(
+				inputs=inputs,
+				units=units,
+				activation=activation,
+				use_bias=use_bias,
+				kernel_initializer=kernel_initializer,
+			)
+
+			return output
+
+	@layer
+	def resize_bilinear(self, inputs, size, name):
+		return tf.image.resize_bilinear(
+			images=inputs,
+			size=size,
+			align_corners=True,
+			name=name,
+		)
+
+	@layer
+	def d_conv(self, inputs, filters, rate=1, strides=[1, 1, 1, 1], padding='SAME',
+			   activation=DEFAULT_ACTIVATION, use_bias=False, kernel_initializer=DEFAULT_INITIALIZER,
+			   name=None, reuse=tf.AUTO_REUSE):
+		with tf.variable_scope(name):
+			kernel = tf.get_variable(name='kernel', shape=filters, initializer=kernel_initializer)
+
+			x = tf.nn.atrous_conv2d(
+				inputs=inputs,
+				kernel=kernel,
+				rate=rate,
+				padding=padding,
+			)
+
+			if use_bias:
+				bias = tf.get_variable(name='bias', shape=[filters[3]], initializer=tf.constant_initializer(value=0))
+				x = tf.nn.bias_add(x, bias)
+
+			if activation is not None:
+				x = activation(x)
+
+			return x
+
+	@layer
+	def resize_nn(self, inputs, size, name):
+		return tf.image.resize_nearest_neighbor(images=inputs, size=size, name=name)
