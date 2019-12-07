154a6e82d3012f8a63304eebd73d7d21e05770cc
Yeojin Kim <yeojin.dev@gmail.com>
2019-12-07 22:57:48 +0900

    SMIT - ICNet attribute 파일 생성 코드 수정

--- smit/icnet/make_attribute.py
+++ smit/icnet/make_attribute.py
@@ -1,18 +1,20 @@
 import os
 
-IMAGE_ROOT = './cityscapes/images/train/aachen'
-LABEL_ROOT = './cityscapes/labels/train/aachen'
+IMAGE_ROOT = './cityscapes/leftImg8bit/{}'
+LABEL_ROOT = './cityscapes/gtFine/{}'
 
-with open('./cityscapes/trainAttribute.txt', 'w') as f:
-	for images, labels in zip(os.walk(IMAGE_ROOT), os.walk(LABEL_ROOT)):
-		image_files = sorted(images[2])
-		label_files = sorted([label_file for label_file in labels[2] if label_file.endswith('labelIds.png')])
+for mode in ('train', 'val'):
 
-		images_abs_root = images[0]
-		labels_abs_root = labels[0]
+	with open(f'./cityscapes/{mode}Attribute.txt', 'w') as f:
+		for images, labels in zip(os.walk(IMAGE_ROOT.format(mode)), os.walk(LABEL_ROOT.format(mode))):
+			image_files = sorted(images[2])
+			label_files = sorted([label_file for label_file in labels[2] if label_file.endswith('labelTrainIds.png')])
 
-		for image_file, label_file in zip(image_files, label_files):
-			image_filepath = os.path.join(os.path.abspath(images_abs_root), image_file)
-			label_filepath = os.path.join(os.path.abspath(labels_abs_root), label_file)
+			images_abs_root = images[0]
+			labels_abs_root = labels[0]
 
-			f.writelines(f'{image_filepath} {label_filepath}\n')
+			for image_file, label_file in zip(image_files, label_files):
+				image_filepath = os.path.join(os.path.abspath(images_abs_root), image_file)
+				label_filepath = os.path.join(os.path.abspath(labels_abs_root), label_file)
+
+				f.writelines(f'{image_filepath} {label_filepath}\n')
